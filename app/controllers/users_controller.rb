6905c9cf73aebaaa244cda9a11d7f29396f50731
Shohey Tanaka <shohei-tanaka@dac.co.jp>
2017-06-21 05:48:52 +0000

    for heroku

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @items = @user.items.uniq
+    @count_want = @user.want_items.count
+    @count_have = @user.have_items.count
+  end
+
+  def new
+    @uesr = User.new
+    @items = @user.items.uniq
+    @count_want = @user.want_items.count
+    @count_have = @user.have_items.count
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = 'ユーザを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    
+  end
+end
